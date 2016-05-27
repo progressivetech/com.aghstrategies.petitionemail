ae66556e2779b1d5226a66a14fd2eae99111ec0d
Andrew Hunt <andrew@aghstrategies.com>
2016-05-27 17:52:54 -0400

    JS changes for petition admin in 4.7

--- js/messageField.js
+++ js/messageField.js
@@ -1,10 +1,20 @@
 CRM.$(function($) {
-  var $messageField = $('#customData .custom-group-Letter_To input[data-crm-custom="Letter_To:Message_Field"]');
-  $messageField.attr({
-    placeholder: '- Select Field -',
-    allowClear: 'true',
+  var msgFieldSelector = '#customData .custom-group-Letter_To input[data-crm-custom="Letter_To:Message_Field"]';
+  var $messageField = $(msgFieldSelector);
+  var initMessageField = function(m) {
+    $messageField.attr({
+      placeholder: '- Select Field -',
+      allowClear: 'true',
+    });
+    createEntityRef(m, $('#profile_id').val());
+  };
+  initMessageField($messageField);
+
+  // 4.6 doesn't trigger crmLoad while 4.7 loads the custom data after this runs
+  $('body').on('crmLoad', function() {
+    $messageField = $(msgFieldSelector);
+    initMessageField($messageField);
   });
-  createEntityRef($messageField, $('#profile_id').val());
 
   $('#profile_id').change( function() {
     $messageField.crmEntityRef('destroy');
@@ -13,22 +23,6 @@ CRM.$(function($) {
   });
 
   function createEntityRef($field, profileId) {
-    noteFields = getNoteFields();
-
-    $field.crmEntityRef({
-      entity: 'UFField',
-      placeholder: '- Select Field -',
-      api: {
-        params: {
-          uf_group_id: profileId,
-          field_name: {"IN": noteFields}
-        }
-      },
-      select: {minimumInputLength: 0},
-    });
-  }
-
-  function getNoteFields() {
     var noteFields = ['activity_details'];
     CRM.api3('CustomGroup', 'get', {
       sequential: 1,
@@ -41,9 +35,20 @@ CRM.$(function($) {
       result['values'].forEach(function(val) {
         val['api.CustomField.get']['values'].forEach(function(field) {
           noteFields.push(field.name);
+          noteFields.push('custom_' + field.id);
         });
       });
-      return noteFields;
+      $field.crmEntityRef({
+        entity: 'UFField',
+        placeholder: '- Select Field -',
+        api: {
+          params: {
+            uf_group_id: profileId,
+            field_name: {"IN": noteFields}
+          }
+        },
+        select: {minimumInputLength: 0},
+      });
     });
   }
 });
