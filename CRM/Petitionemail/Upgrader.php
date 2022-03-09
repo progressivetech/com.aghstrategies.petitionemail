6b0ac8af657c6d781043bcdda62d4f4574e39043
Jamie McClelland <jm@mayfirst.org>
2022-03-09 09:45:17 -0500

    don't rip out the message field until it's been replaced

--- CRM/Petitionemail/Upgrader.php
+++ CRM/Petitionemail/Upgrader.php
@@ -8,31 +8,5 @@ use Civi\Api4\UFField;
  */
 class CRM_Petitionemail_Upgrader extends CRM_Petitionemail_Upgrader_Base {
 
-  /**
-   * Example: Run a couple simple queries.
-   *
-   * @return TRUE on success
-   * @throws Exception
-   */
-  public function upgrade_1000() {
-    $this->ctx->log->info('Deleting "Message_Field" custom field');
-    $customField = CustomField::get(FALSE)
-      ->addWhere('custom_group_id:name', '=', 'Letter_To')
-      ->addWhere('name', '=', 'Message_Field')
-      ->execute()
-      ->first();
-    if (!empty($customField)) {
-      $customFieldName = "custom_{$customField['id']}";
-      // Delete field from Profiles
-      UFField::delete(FALSE)
-        ->addWhere('field_name', '=', $customFieldName)
-        ->execute();
-      // Now delete the custom field
-      CustomField::delete(FALSE)
-        ->addWhere('custom_group_id:name', '=', 'Letter_To')
-        ->addWhere('name', '=', 'Message_Field')
-        ->execute();
-    }
-    return TRUE;
-  }
+  
 }
