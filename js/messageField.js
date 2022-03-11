60996200d9044d665431668583c119dab8926859
Jamie McClelland <jm@mayfirst.org>
2022-03-11 14:02:22 -0500

    rip out old fields, add new ones.
    This is a mid-commit during a major refactor.

--- js/messageField.js
+++ js/messageField.js
@@ -1,53 +1,106 @@
 CRM.$(function($) {
-  var msgFieldSelector = '#customData .custom-group-Letter_To input[data-crm-custom="Letter_To:Message_Field"]';
-  var $messageField = $(msgFieldSelector);
-  var initMessageField = function(m) {
-    $messageField.attr({
+  var msgFieldSelector = 'input[data-crm-custom="Letter_To:Message_Field"]';
+  var subFieldSelector = 'input[data-crm-custom="Letter_To:Subject_Field"]';
+  var recipientSystemFieldSelector = 'select[data-crm-custom="Letter_To:Recipient_System"]';
+
+  // This field is populated on load.
+  var recipientSystemField = null;
+  var singleFields = [ 
+    'To', 
+  ]; 
+
+  var initEntityFieldDropDown = function(f, dataType) {
+    f.attr({
       placeholder: '- Select Field -',
       allowClear: 'true',
     });
-    createEntityRef(m, $('#profile_id').val());
-  };
-  initMessageField($messageField);
 
-  // 4.6 doesn't trigger crmLoad while 4.7 loads the custom data after this runs
-  $('body').on('crmLoad', function() {
-    $messageField = $(msgFieldSelector);
-    initMessageField($messageField);
-  });
+    // Create a variable of all possible fields.
+    var fields = [];
+    if (dataType == 'Memo') {
+      fields.push('activity_details');
+    }
+    else if (dataType == 'String') {
+      fields.push ('activity_subject');
+    }
 
-  $('#profile_id').change( function() {
-    $messageField.crmEntityRef('destroy');
-    $messageField.val('');
-    createEntityRef($messageField, $('#profile_id').val());
-  });
-
-  function createEntityRef($field, profileId) {
-    var noteFields = ['activity_details'];
+    // Get a list of custom field groups that extend activities. 
     CRM.api3('CustomGroup', 'get', {
       sequential: 1,
       extends: 'Activity',
       'api.CustomField.get': {
-        data_type: 'Memo',
+        data_type: dataType,
         return: 'name'
       }
     }).done(function(result) {
+      console.log(result);
       result['values'].forEach(function(val) {
         val['api.CustomField.get']['values'].forEach(function(field) {
-          noteFields.push('custom_' + field.id);
+          fields.push('custom_' + field.id);
         });
       });
-      $field.crmEntityRef({
+      console.log("Fields are", fields);
+      f.crmEntityRef({
         entity: 'UFField',
         placeholder: '- Select Field -',
         api: {
           params: {
-            uf_group_id: profileId,
-            field_name: {"IN": noteFields}
+            uf_group_id: CRM.$('#profile_id').val(),
+            field_name: {"IN": fields}
           }
         },
         select: {minimumInputLength: 0},
       });
     });
-  }
+  };
+
+  var displaySingleFields = function() {
+    var field;
+    for(var key in singleFields) {
+      field = singleFields[key];
+      CRM.$('input[data-crm-custom="Letter_To:' + field + '"]').parent().parent().show();
+    }
+  };
+
+  var hideSingleFields = function () {
+    var field;
+    for(var key in singleFields) {
+      field = singleFields[key];
+      CRM.$('input[data-crm-custom="Letter_To:' + field + '"]').parent().parent().hide();
+    }
+  };
+
+  var toggleSingleFields = function () {
+    if (recipientSystemField.val() == 'Single') {
+      displaySingleFields();
+    }
+    else {
+      hideSingleFields();
+    }
+  };
+  var createEntityRef = function(field, profileId, dateType) {
+    
+  };
+
+  CRM.$('body').on('crmLoad', function() {
+    recipientSystemField = CRM.$(recipientSystemFieldSelector);
+    messageField = CRM.$(msgFieldSelector);
+    subjectField = CRM.$(subFieldSelector);
+    initEntityFieldDropDown(messageField, 'Memo');
+    initEntityFieldDropDown(subjectField, 'String');
+    toggleSingleFields();
+    recipientSystemField.change( function() {
+      toggleSingleFields();
+      
+    });
+    CRM.$('#profile_id').change( function() {
+      var messageField = CRM.$(msgFieldSelector);
+      messageField.val('');
+      createEntityRef(messageField, CRM.$('#profile_id').val());
+    });
+  });
+
+  
+
+  
 });
